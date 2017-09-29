96cad817fc7a61c067d2f36d7d944d8a040095d3
cousincarrot <31780818+cousincarrot@users.noreply.github.com>
2017-09-29 16:20:02 -0500

    testing array

--- projectB.js
+++ projectB.js
@@ -1,9 +1,10 @@
 //To Do: Point to the .txt doc wih the array vlues in it
 //set the values of that doc in an array
 //merge sort the values of that array
-//console.log a specific piece of data from the array
-
-function readTextFile("file:///D:/Documents/coding_projects/git_repositories/ProjectB/IntegerArray.txt") {
+//console.log the computed the number of inversions in the file given,
+//where the ith row of the file indicates the ith entry of an array
+/*
+function readTextFile("https://lagunita.stanford.edu/asset-v1:Engineering+Algorithms1+SelfPaced+type@asset+block/IntegerArray.txt") {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", "IntegerArray.txt", false);
     rawFile.onreadystatechange = function ()
@@ -19,3 +20,14 @@ function readTextFile("file:///D:/Documents/coding_projects/git_repositories/Pro
     }
     rawFile.send(null);
 }
+*/
+
+//mergesort test
+sortTest = new Array();
+
+sortTest[0] = 20;
+sortTest[1] = 40;
+sortTest[2] = 30;
+sortTest[4] = 10;
+
+console.log(sortTest.length);

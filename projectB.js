6522f97dfac20deb59d3643ea86beaffd9c61f07
cousincarrot <31780818+cousincarrot@users.noreply.github.com>
2017-10-02 14:38:54 -0500

    This sorts but I'm not sure it tallies correctly

--- projectB.js
+++ projectB.js
@@ -22,12 +22,57 @@ function readTextFile("https://lagunita.stanford.edu/asset-v1:Engineering+Algori
 }
 */
 
-//mergesort test
-sortTest = new Array();
-
+//creating a simple array to test mergesort functionality
+sortTest = new Array(6, 5, 4, 3, 2, 1);
+/*
 sortTest[0] = 20;
 sortTest[1] = 40;
 sortTest[2] = 30;
-sortTest[4] = 10;
-
+sortTest[3] = 10;
+*/
 console.log(sortTest.length);
+
+//setting a tally for inversions
+var tally = 0;
+
+//mergesort
+// Split the array into halves and merge them recursively
+function mergeSort (sortTest) {
+  if (sortTest.length === 1) {
+    // return once we hit an array with a single item
+    return sortTest;
+  }
+
+  sortTest.middle = Math.floor(sortTest.length / 2) // get the middle item of the array rounded down
+  sortTest.left = sortTest.slice(0, sortTest.middle) // items on the left side
+  sortTest.right = sortTest.slice(sortTest.middle) // items on the right side
+
+  return merge(
+    mergeSort(sortTest.left),
+    mergeSort(sortTest.right)
+  )
+};
+
+// compare the arrays item by item and return the concatenated result
+function merge (left, right) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+      tally++
+    }
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+};
+
+
+console.log(mergeSort(sortTest)); // [10, 20, 30, 40]
+console.log(tally);
